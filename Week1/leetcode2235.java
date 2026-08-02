329c69791173618dd778db0bff33857d734256e4
Vishal Khandelwal <158454202+vishalkhandelwal516@users.noreply.github.com>
2026-08-02 18:49:41 +0530

    Add files via upload

---
+++ Week1/leetcode2235.java
@@ -0,0 +1,13 @@
+public class leetcode2235 {
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+}
+// These are the answers from this question mentioned at the last of this task problem:-
+// - num1 and num2 are 32-bit sized integers. They can accept positive, negative values and zero as well
+// - Int return method returns a single 32-bit signed integer representing the sum of num1 and num2.
+// - The scanner and main method is leetCode's underlying system supplies in its own driver.
+// - doubt
